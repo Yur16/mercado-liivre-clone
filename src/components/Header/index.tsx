df6eb78603f534fb4294e550e1641d844093da57
Yur16 <45566439+Yur16@users.noreply.github.com>
2020-09-08 14:29:13 -0300

    style estilização do header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,73 @@
 import React from "react";
+import { GrLocation } from "react-icons/gr";
+import {
+  AiOutlineSearch,
+  AiOutlineThunderbolt,
+  AiOutlineShoppingCart
+} from "react-icons/ai";
 
-import { Container } from "./styles";
+import MercadoLivreLogo from "../../assets/mercado-livre.png";
+
+import {
+  Container,
+  LogoContainer,
+  Cep,
+  SearchContainer,
+  Search,
+  Tabs,
+  SignContainer
+} from "./styles";
 
 const Header: React.FC = () => {
-  return <Container>Interface reproduzida com a Rocketseat</Container>;
+  return (
+    <Container>
+      <LogoContainer>
+        <img src={MercadoLivreLogo} alt="mercado livre" />
+        <Cep>
+          <GrLocation size={30} color="#000" />
+          <span>
+            Infrome seu <strong>CEP</strong>
+          </span>
+        </Cep>
+      </LogoContainer>
+
+      <SearchContainer>
+        <Search>
+          <input
+            type="text"
+            placeholder="Buscar produtos, marcas e muito mais..."
+          />
+          <button type="submit">
+            <AiOutlineSearch size={20} color="#7b7b7b" />
+          </button>
+        </Search>
+
+        <Tabs>
+          <a href="#">Categorias</a>
+          <a href="#">Ofertas do dia</a>
+          <a href="#">Histórico</a>
+          <a href="#">Supermercados</a>
+          <a href="#">Lojas oficiais</a>
+          <a href="#">Vender</a>
+          <a href="#">Contato</a>
+        </Tabs>
+      </SearchContainer>
+
+      <SignContainer>
+        <div className="message">
+          <AiOutlineThunderbolt size={35} color="#000" />
+          <span>Aproveite preços incríveis todos os dias</span>
+        </div>
+
+        <div className="tabs">
+          <AiOutlineShoppingCart size={25} color="#000" />
+          <a href="#">Compras</a>
+          <a href="#">Entre</a>
+          <a href="#">Crie sua conta</a>
+        </div>
+      </SignContainer>
+    </Container>
+  );
 };
 
 export default Header;
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -2,11 +2,124 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   width: 100%;
-  height: 48px;
   background: var(--color-header);
   border-bottom: 1px solid var(--color-border);
 
+  display: grid;
+  grid-gap: 50px;
+  grid-template-columns: 1fr 4.5fr 2.5fr;
+
+  padding: 8px 80px;
+`;
+export const LogoContainer = styled.div`
   display: flex;
-  align-items: center;
-  justify-content: center;
+  flex-direction: column;
+
+  > img {
+    width: 100%;
+    height: 35px;
+  }
+`;
+
+export const Cep = styled.div`
+  display: flex;
+  flex-direction: row;
+
+  max-width: 100px;
+
+  margin-top: 15px;
+
+  > span {
+    color: var(--color-black);
+    font-size: 11px;
+    font-weight: 200;
+
+    > strong {
+      font-size: 13px;
+      font-weight: 500;
+    }
+  }
+`;
+
+export const SearchContainer = styled.div`
+  width: 100%;
+`;
+
+export const Search = styled.div`
+  display: flex;
+  background: #fff;
+
+  width: 100%;
+  height: 40px;
+
+  border-radius: 2px;
+
+  border-bottom: 1px solid var(--color-border);
+  box-shadow: 0px 2px 3px -3px var(--color-black);
+
+  > input {
+    flex: 1;
+    padding: 0 15px;
+    border: 0;
+    font-size: 14px;
+
+    ::placeholder {
+      color: var(--color-search-placeholder);
+    }
+  }
+
+  > button {
+    padding: 10px;
+    border-left: 1px solid var(--color-border);
+  }
+`;
+
+export const Tabs = styled.div`
+  display: flex;
+  flex-direction: row;
+
+  margin-top: 15px;
+  padding: 5px 0;
+
+  > a {
+    color: var(--color-black);
+    font-size: 13px;
+    text-decoration: none;
+
+    margin-right: 15px;
+    opacity: 0.7;
+  }
+`;
+
+export const SignContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  .message {
+    display: flex;
+    align-items: center;
+    align-self: flex-end;
+
+    > span {
+      font-size: 15px;
+      color: var(--color-black);
+      font-weight: 500;
+    }
+  }
+
+  .tabs {
+    display: flex;
+    align-items: center;
+    flex-direction: row-reverse;
+
+    margin-top: 20px;
+
+    > a {
+      color: var(--color-black);
+      font-size: 13px;
+      text-decoration: none;
+
+      margin-right: 15px;
+    }
+  }
 `;
